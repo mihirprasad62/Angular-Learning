2ba5786f7add9636f508fb4041dd8f7165d6230b
mihir Prasad bhuyan <mihirprasad62@gmail.com>
2025-09-26 10:24:05 +0530

    Components in Angular Completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./login/login";
+import { SignUp } from "./sign-up/sign-up";
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login, SignUp,Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-profile',
+    // template:`<h1>Profile Component</h1>`,
+    // styles:'h1{color:orange}',
+    templateUrl:'./profile.html',
+    styleUrl:'./profile.css'
+})
+
+export class Profile{
+
+}
