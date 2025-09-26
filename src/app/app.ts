17424777f370987d351d09f66bb35a8c30b2b249
mihir Prasad bhuyan <mihirprasad62@gmail.com>
2025-09-26 11:36:22 +0530

    Counter Application Made

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,50 +1,63 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from "./login/login";
-import { SignUp } from "./sign-up/sign-up";
+import { Login } from './login/login';
+import { SignUp } from './sign-up/sign-up';
 import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, SignUp,Profile],
+  imports: [RouterOutlet, Login, SignUp, Profile],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('Angular_Learning');
-  name="Mihir Prasad Bhuyan";
-  x=10;
-  y=20;
-  user1="Sameer";
-  user2="Bapi"
+  // protected readonly title = signal('Angular_Learning');
+  // name="Mihir Prasad Bhuyan";
+  // x=10;
+  // y=20;
+  // user1="Sameer";
+  // user2="Bapi"
 
-  userName:string="Mihir"
-  data:string|number='Hello'
-  other:any=10
+  // userName:string="Mihir"
+  // data:string|number='Hello'
+  // other:any=10
 
-  handleClickEvent(){
-   console.log('Function Called.')
-   this.handleOtherFunction()
+  // handleClickEvent(){
+  //  console.log('Function Called.')
+  //  this.handleOtherFunction()
+  // }
+
+  // handleOtherFunction(){
+  //   console.log("other function called.")
+  // }
+  // updateUsername(){
+  //   this.userName='Sameer'
+  //   this.data=30
+  //   this.other=true
+  // }
+
+  // updateVar(){
+  //   let x:number=30;
+
+  //   console.log(x)
+  //   this.sum(50,50)
+  // }
+
+  // sum(a:number,b:number){
+  //   console.log(a+b)
+  // }
+
+  count: number = 0;
+
+  handleIncrement() {
+    this.count += 1;
   }
-
-  handleOtherFunction(){
-    console.log("other function called.")
+  handleDecrement() {
+    if (this.count > 0) {
+      this.count -= 1;
+    }
   }
-  updateUsername(){
-    this.userName='Sameer'
-    this.data=30
-    this.other=true
+  handleReset() {
+    this.count = 0;
   }
-
-  updateVar(){
-    let x:number=30;
-
-    console.log(x)
-    this.sum(50,50)
-  }
-
-  sum(a:number,b:number){
-    console.log(a+b)
-  }
-
 }
